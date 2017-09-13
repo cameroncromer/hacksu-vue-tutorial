023aeacdf00e85a5e6528d8f21d05a3aa4a007e1
Cameron Cromer <ccromer3@kent.edu>
2017-09-12 20:46:24 -0400

    Add generated code with minor modifications

---
+++ suggestion-list/src/components/Hello.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+    <input type="text" placeholder="Input new suggestion" v-model="newSuggestion" @keypress.enter="submitSuggestion">
+    <ul>
+      <li v-for="suggestion in suggestions">{{ suggestion.suggestionText }} <i>{{ suggestion.votes }}</i></li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      newSuggestion: '',
+      suggestions: [
+        {
+          suggestionText: 'Nick needs outta here',
+          votes: 1
+        }
+      ]
+    }
+  },
+  methods: {
+    submitSuggestion () {
+      var suggestion = {
+        suggestionText: this.data.newSuggestion,
+        votes: 0
+      }
+      this.data.suggestions.push(suggestion)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
